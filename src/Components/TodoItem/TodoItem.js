3d57c131ec3d7ef6413ad1a8839454c0d4c9e085
MarinSt <st_marinh@protonmail.com>
2022-04-06 13:15:32 -0500

    First version CSS style

--- src/Components/TodoItem/TodoItem.js
+++ src/Components/TodoItem/TodoItem.js
@@ -1,11 +1,12 @@
+import "./TodoItem.css";
 
 
 function TodoItem(props) {
     return (
-        <li>
-            <span></span>
-            <p>{props.text}</p>
-            <span></span>
+        <li className="item">
+            <span className="icon icon-check">/</span>
+            <p className="item-p">{props.text}</p>
+            <span className="icon icon-delete">X</span>
         </li>
     );
 }
