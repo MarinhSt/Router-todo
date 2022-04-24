bbe8b8668958cdb3cf782ef728e512e3e08222ab
MarinhSt <st_marinh@protonmail.com>
2022-04-23 19:54:51 -0500

    Fixed localStorage update issue. The key of the setItem and getItem localStorage method has been set to sting when it is a variable.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { TodoCounter } from '../TodoCounter/TodoCounter';
-import { TodoSearch } from '../TodoSearch/TodoSearch';
-import { TodoList } from '../TodoList/TodoList';
-import { TodoItem } from '../TodoItem/TodoItem';
+import React from "react";
+import { TodoCounter } from "../TodoCounter/TodoCounter";
+import { TodoSearch } from "../TodoSearch/TodoSearch";
+import { TodoList } from "../TodoList/TodoList";
+import { TodoItem } from "../TodoItem/TodoItem";
 
 // const defaultTodos = [
 //   { text: 'say hey', status: true, },
@@ -13,109 +13,106 @@ import { TodoItem } from '../TodoItem/TodoItem';
 // ]
 
 function useLocalStorage(itemName, initialValue) {
-
   //state of error on page
-  const [error, setError] = React.useState(false)
+  const [error, setError] = React.useState(false);
   //state of load page
-  const [loading, setLoading] = React.useState(true)
+  const [loading, setLoading] = React.useState(true);
   //state of to-dos
-  const [item, setItem] = React.useState(initialValue)
-
+  const [item, setItem] = React.useState(initialValue);
 
   React.useEffect(() => {
     // setTimeout used to simulate the request to API
     setTimeout(() => {
       try {
-
         // local storage was used as the database for the persistance the data
-        const localItem = localStorage.getItem(itemName)
-        let parsedItem
+        const localItem = localStorage.getItem(itemName);
+        let parsedItem;
 
         // verification the existence of item in local storage
-        if (!localStorage.getItem('itemName')) {
-          localStorage.setItem('itemName', JSON.stringify(initialValue))
-          parsedItem = initialValue
+        if (!localStorage.getItem(itemName)) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
         } else {
-          parsedItem = JSON.parse(localItem)
+          parsedItem = JSON.parse(localItem);
         }
 
-        setItem(parsedItem)
-        setLoading(false)
-
+        setItem(parsedItem);
+        setLoading(false);
       } catch (error) {
-        setError(error)
+        setError(error);
       }
-
     }, 1000);
     /* if I leave this empty, when I like to modify (complete or delete) to-dos; them will be reappear again  */
     // review this, have a problems to change to-dos; don't update changes into localStorage.
-  }, [])
+  });
 
   // save change of components in local storage
   const saveItem = (newItem) => {
     try {
-      localStorage.setItem('itemName', JSON.stringify(newItem))
-      setItem(newItem)
+      localStorage.setItem(itemName, JSON.stringify(newItem));
+      setItem(newItem);
     } catch (error) {
-      setError(error)
+      setError(error);
     }
-  }
+  };
 
-  return { item, saveItem, loading, error }
+  return { item, saveItem, loading, error };
 }
 
 function App() {
   // hook useLocalStorage
-  const { item: todos, saveItem: saveTodos, loading, error } = useLocalStorage('TODOS_LOCAL', [])
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage("TODOS_LOCAL", []);
 
   //state of  input search value
-  const [searchValue, setSearchValue] = React.useState('')
+  const [searchValue, setSearchValue] = React.useState("");
 
   //variables to know the status of to-dos
-  const completedTodos = todos.filter(todo => todo.status).length
-  const totalTodos = todos.length
+  const completedTodos = todos.filter((todo) => todo.status).length;
+  const totalTodos = todos.length;
 
   // according to the value of input search filters the to-dos.
-  let showTodos = todos.filter(todo => todo.text.toLowerCase().includes(searchValue))
+  let showTodos = todos.filter((todo) =>
+    todo.text.toLowerCase().includes(searchValue)
+  );
 
   // state change functions of to-dos
   const completeTodo = (text) => {
-    const index = todos.findIndex(todo => todo.text === text)
-    const newTodos = [...todos]
+    const index = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
     // mark as completed or pending
-    newTodos[index].status !== true ? newTodos[index].status = true : newTodos[index].status = false
-    saveTodos(newTodos)
-  }
+    newTodos[index].status !== true
+      ? (newTodos[index].status = true)
+      : (newTodos[index].status = false);
+    saveTodos(newTodos);
+  };
 
   const deleteTodo = (text) => {
-    const index = todos.findIndex(todo => todo.text === text)
-    const newTodos = [...todos]
+    const index = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
     // mark as completed or pending
-    newTodos.splice(index, 1)
-    saveTodos(newTodos)
-  }
+    newTodos.splice(index, 1);
+    saveTodos(newTodos);
+  };
 
   return (
     <>
       <header>
-        <h1 className='title'>This are yours tasks</h1>
+        <h1 className="title">This are yours tasks</h1>
       </header>
-      <TodoCounter
-        total={totalTodos}
-        completed={completedTodos}
-      />
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
+      <TodoCounter total={totalTodos} completed={completedTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
-
         {/* state to make know to user status of the page. */}
-        {error && 'Error to load data'}
-        {loading && 'Loading data...'}
-        {(!loading && !totalTodos) && 'Organize your time. Place your tasks...'}
+        {error && "Error to load data"}
+        {loading && "Loading data..."}
+        {!loading && !totalTodos && "Organize your time. Place your tasks..."}
 
-        {showTodos.map(todo => (
+        {showTodos.map((todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}
